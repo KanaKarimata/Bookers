05dcff0d4fb02ee34bb2af407b99b199cc108203
EC2 Default User <ec2-user@ip-172-31-95-22.ec2.internal>
2022-03-19 08:59:54 +0000

    [Add]新規投稿機能

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,29 @@
 class HomesController < ApplicationController
   def top
   end
+  
+  def index
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@list.id)
+    else
+      
+    end
+  
+  def show
+    
+  end
+  
+  def edit
+  end
+  
+  private
+  def book_params
+    params. require(:book).permit(:title, :body)
+  end
+  
 end
